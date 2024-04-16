1a7d934cf651b06a69654c2409cbc759f99861eb
gl7857 <gl7857@bs.amalnet.k12.il>
2024-04-16 23:51:14 +0300

    demo13

--- app/src/main/java/com/example/demo13/result.java
+++ app/src/main/java/com/example/demo13/result.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class result extends AppCompatActivity {
