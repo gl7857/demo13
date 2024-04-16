213987edfdaceae8087c49e9f455246cee084de2
gl7857 <gl7857@bs.amalnet.k12.il>
2024-04-16 23:50:43 +0300

    demo13

--- app/src/main/java/com/example/demo13/MainActivity.java
+++ app/src/main/java/com/example/demo13/MainActivity.java
@@ -1,3 +1,5 @@
+package com.example.demo13;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -5,7 +7,6 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
